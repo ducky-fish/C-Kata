f80e93ed4ecf979e2c3f7ea4f70e03f771547f09
ducky-fish <35616959+ducky-fish@users.noreply.github.com>
2021-09-20 18:39:19 -0600

    Update disemvowel.cpp
    Changed 
    'found = fixed.find_first_of("aieouAEIOU",found+1);'
    ↓
    'found = fixed.find_first_of("aieouAEIOU",found);'
    ___
         This updates the operation to remove vowels that are directly next to one another such as in words like 'your'

--- disemvowel.cpp
+++ disemvowel.cpp
@@ -7,30 +7,10 @@ std::string disemvowel(const std::string& str) {
   std::string fixed (str);
   std::size_t found = str.find_first_of("aeiouAEIOU");
   
-  std::cout << found << '\n';
-  std::cout << fixed << '\n';
   while(found != std::string::npos)
   {
     fixed.erase(fixed.begin()+found); 
-    std::cout << fixed << '\n';
-    found = fixed.find_first_of("aieouAEIOU",found+1);
-    std::cout << found << '\n';
+    found = fixed.find_first_of("aieouAEIOU",found);
   }
   return fixed;
 }
-
-/*"This website is for losers LOL!"
-     2   6  9 11 13  17 21 23 28
-"Ths website is for losers LOL!"
-      5->7
-"Ths wbsite is for losers LOL!"
-        7
-"Ths wbste is for losers LOL!"
-         8
-"Ths wbst is for losers LOL!"
-          9
-"Ths wbst s for losers LOL!"
-             12
-
-*/
-
